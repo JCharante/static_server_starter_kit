b3bf5427e2d7d61ea6b00e46670147f15a862d56
John <jcharante@gmail.com>
2017-01-26 15:59:05 -0500

    Updated server.py, now prints port

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
+port = 4847
 app = Flask(__name__)
 
 @app.route('/')
 def index():
   return 'Serving Static Files with Flask + Nginx. https://github.com/JCharante/static_server_starter_kit'
-  
+ 
+ print(f'Starting static flask service, on port {port}')
+
  app.run(debug=True, host='0.0.0.0', port=4847, threaded=True)
  
