199f79b61438d65e525e6e1b0b5b173616d8d206
John <jcharante@gmail.com>
2017-01-26 16:08:28 -0500

    Fixed indentation in server.py

--- server.py
+++ server.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
   return 'Serving Static Files with Flask + Nginx. https://github.com/JCharante/static_server_starter_kit'
- 
- print(f'Starting static flask service, on port {port}')
 
- app.run(debug=True, host='0.0.0.0', port=port, threaded=True)
- 
+
+print(f'Starting static flask service, on port {port}')
+
+app.run(debug=True, host='0.0.0.0', port=port, threaded=True)
